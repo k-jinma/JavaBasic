f66f2378a2f016137386737c5dbd103a52c48705
k-jinma <jinmakazuhiro@outlook.jp>
2021-05-01 20:16:21 +0900

    System.out.print() 追記

--- B001/src/App.java
+++ B001/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.print(100);
+        System.out.println("円");
     }
 }
