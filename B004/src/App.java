04cb722ca018771e2b7bb51ba59dc3228b2259cc
k-jinma <jinmakazuhiro@outlook.jp>
2021-05-04 00:34:16 +0900

    Basic Chapter4

---
+++ B004/src/App.java
@@ -0,0 +1,87 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int score = 90;
+        if(score >= 80){
+            System.out.println("合格です");
+        }
+        System.out.println("処理を終了します");
+
+        //「if文」
+        // int score = 70;
+        // if(score >= 80){
+        //     System.out.println("合格です");
+        // }
+        // System.out.println("処理を終了します");
+
+        //「if～else文」
+        // int score = 70;
+        // if(score >= 80){
+        //     System.out.println("合格です");
+        // }else if(score >= 60){
+        //     System.out.println("補欠合格です");
+        // }
+        // System.out.println("処理を終了します");
+
+        //「else文」
+        // int score = 50;
+        // if(score >= 80){
+        //     System.out.println("合格です");
+        // }else if(score >= 60){
+        //     System.out.println("補欠合格です");
+        // }else{
+        //     System.out.println("不合格です");
+        // }
+        // System.out.println("処理を終了します");
+
+        //「if文のネスト」
+        // int score = 120;
+        // if (score <= 100) {
+        //     if (score >= 80) {
+        //         System.out.println("合格です");
+        //     } else if (score >= 60) {
+        //         System.out.println("補欠合格です");
+        //     } else {
+        //         System.out.println("不合格です");
+        //     }
+        // }
+        // System.out.println("処理を終了します");
+
+        //「論理演算子&&」
+        // int math = 80;
+        // int english = 50;
+        // if (math >= 80 && english >= 80) {
+        //     System.out.println("合格です");
+        // } else {
+        //     System.out.println("不合格です");
+        // }
+        // System.out.println("処理を終了します");
+
+        //「論理演算子||」
+        // int math = 80;
+        // int english = 50;
+        // if (math >= 80 || english >= 80) {
+        //     System.out.println("合格です");
+        // } else {
+        //     System.out.println("不合格です");
+        // }
+        // System.out.println("処理を終了します");
+
+        //「switch文」
+        // char moji ='a';
+        // switch(moji){
+        //     case 'a':
+        //         System.out.println("aです");
+        //         break;
+        //     case 'b':
+        //         System.out.println("bです");
+        //         break;
+        //     case 'c':
+        //         System.out.println("cです");
+        //         break;
+        //     default:
+        //         System.out.println("cです");
+        //         break;
+        // }
+
+    }
+}
