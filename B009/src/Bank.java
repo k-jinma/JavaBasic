9bbb8fe7a845d42e318b9f53edd1665091ca9f35
k-jinma <jinmakazuhiro@outlook.jp>
2021-05-11 21:57:17 +0900

    Basic Chapter9

---
+++ B009/src/Bank.java
@@ -0,0 +1,9 @@
+public class Bank {
+    private int money;
+    public void addMoney( int money ){
+        this.money += money;
+    }
+    public int getMoney(){
+        return this.money;
+    }
+}
---
+++ B009/src/UseBank.java
@@ -0,0 +1,15 @@
+public class UseBank {
+    public static void main(String[] args) throws Exception {
+        Bank bank = new Bank();
+        int money = 10000;
+        //bank.moneyはprivate修飾子が付与されているため直接アクセスできない
+        //bank.money = money;
+        System.out.println("振込額:" + money );
+        bank.addMoney(money);
+
+        int nowMoney = bank.getMoney();
+        System.out.println("現在の所持金：" + nowMoney );
+
+
+    }
+}
