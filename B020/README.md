b4350ce5ba4cf48292491446519f978dd1264b64
k-jinma <jinmakazuhiro@outlook.jp>
2021-06-05 01:47:14 +0900

    Basic Chapter20 途中

---
+++ B020/README.md
@@ -0,0 +1,4 @@
+# 20. コレクション
+1. java.util.Listインターフェース
+    1. ArrayList
+    1. LinkedList
---
+++ B020/src/sample/chapter20/ArrayListSample.java
@@ -0,0 +1,22 @@
+package sample.chapter20;
+
+import java.util.*;
+
+public class ArrayListSample {
+    public static void main(String[] args) {
+        String strValue1 = "Java";
+        Integer intValue = 100;
+        Double doubleValue = 10.5;
+        
+        ArrayList array = new ArrayList(20);    //要素数を指定しない場合は10となる
+
+        array.add(strValue1);
+        array.add(intValue);
+        array.add(doubleValue);
+
+        System.out.println("コレクションに格納されている要素：" + array );
+        System.out.println("Index[2]に格納されている要素：" + array.get(2) );
+        System.out.println("コレクション内の要素数：" + array.size() );
+
+    }
+}
